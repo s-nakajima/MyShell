2b48e4053ec95e7f7e644bf2115de618e444777b
s-nakajima <nakajimashouhei@gmail.com>
2019-09-22 18:38:22 +0900

    Modfied pluginTest.

--- nc3PluginTest/PluginAllTest.sh
+++ nc3PluginTest/PluginAllTest.sh
@@ -17,38 +17,39 @@ LOGFILE=${CURDIR}/AllPluginTest.log
 echo "" > ${LOGFILE}
 date 1>> ${LOGFILE} 2>&1
 
-cd ${CURDIR}/../install/
-
-COMMAND="bash install_dirpath.sh develop"
-echo ${COMMAND}
-bash install_dirpath.sh develop 1>> ${LOGFILE} 2>&1
+#cd ${CURDIR}/../install/
+#
+#COMMAND="bash install_dirpath.sh develop"
+#echo ${COMMAND}
+#bash install_dirpath.sh develop 1>> ${LOGFILE} 2>&1
 
 date 1>> ${LOGFILE} 2>&1
 
 cd ${CURDIR}
 
-if [ -f nc3profile ]; then
-. nc3profile
-fi
+#if [ -f nc3profile ]; then
+#. nc3profile
+#fi
 
 
 #TARGET_DIR=/var/www/app
-TARGET_DIR=/var/www/html/nc3
+#TARGET_DIR=/var/www/html/nc3
+TARGET_DIR=/var/www/html/NetCommons3
 
-MAIL_LOG_CMD="scp -i /root/.ssh/id_rsa ${LOGFILE} ${SAKURA_USER}@${SAKURA_HOST}:~/log/nc3-mail.log"
+#MAIL_LOG_CMD="scp -i /root/.ssh/id_rsa ${LOGFILE} ${SAKURA_USER}@${SAKURA_HOST}:~/log/nc3-mail.log"
 #MAIL_LOG_CMD="scp -i /home/vagrant/.ssh/id_rsa ${LOGFILE} ${SAKURA_USER}@${SAKURA_HOST}:~/log/nc3-mail.log"
-
-SEND_MAIL_CMD="ssh -i /root/.ssh/id_rsa ${SAKURA_USER}@${SAKURA_HOST} ${SAKURA_SHELL}/sakura-mail.sh"
+#
+#SEND_MAIL_CMD="ssh -i /root/.ssh/id_rsa ${SAKURA_USER}@${SAKURA_HOST} ${SAKURA_SHELL}/sakura-mail.sh"
 #SEND_MAIL_CMD="ssh -i /home/vagrant/.ssh/id_rsa ${SAKURA_USER}@${SAKURA_HOST} ${SAKURA_SHELL}/sakura-mail.sh"
-
-if [ -f nc3profile ]; then
-	echo ${MAIL_LOG_CMD}
-	${MAIL_LOG_CMD}
-
-	COMMAND="${SEND_MAIL_CMD} 'PUGIN TEST $NOW \"install_dirpath.sh\"'"
-	echo "$COMMAND"
-	${COMMAND}
-fi
+#
+#if [ -f nc3profile ]; then
+#	echo ${MAIL_LOG_CMD}
+#	${MAIL_LOG_CMD}
+#
+#	COMMAND="${SEND_MAIL_CMD} 'PUGIN TEST $NOW \"install_dirpath.sh\"'"
+#	echo "$COMMAND"
+#	${COMMAND}
+#fi
 
 
 PLUGIN_NAME=`ls $TARGET_DIR/app/Plugin`
@@ -89,14 +90,14 @@ do
 
 			date 1>> ${LOGFILE} 2>&1
 
-			if [ -f nc3profile ]; then
-				echo ${MAIL_LOG_CMD}
-				${MAIL_LOG_CMD}
-
-				COMMAND="${SEND_MAIL_CMD} 'PUGIN TEST $NOW \"$testPlugin\"'"
-				echo "$COMMAND"
-				${COMMAND}
-			fi
+#			if [ -f nc3profile ]; then
+#				echo ${MAIL_LOG_CMD}
+#				${MAIL_LOG_CMD}
+#
+#				COMMAND="${SEND_MAIL_CMD} 'PUGIN TEST $NOW \"$testPlugin\"'"
+#				echo "$COMMAND"
+#				${COMMAND}
+#			fi
 
 			testPlugin=""
 			count=0
@@ -115,22 +116,22 @@ if [ ! "$testPlugin" = "" ]; then
 	date 1>> ${LOGFILE} 2>&1
 fi
 
-if [ -f nc3profile ]; then
-	echo ${MAIL_LOG_CMD}
-	${MAIL_LOG_CMD}
-
-	COMMAND="${SEND_MAIL_CMD} 'PUGIN TEST $NOW \"$testPlugin\"'"
-	echo "$COMMAND"
-	${COMMAND}
-
-	cp -pf $RESULT_LOGFILE $LOGFILE
-	echo ${MAIL_LOG_CMD}
-	${MAIL_LOG_CMD}
-
-	COMMAND="${SEND_MAIL_CMD} 'PUGIN TEST $NOW \"Summary results\"'"
-	echo "$COMMAND"
-	${COMMAND}
-fi
+#if [ -f nc3profile ]; then
+#	echo ${MAIL_LOG_CMD}
+#	${MAIL_LOG_CMD}
+#
+#	COMMAND="${SEND_MAIL_CMD} 'PUGIN TEST $NOW \"$testPlugin\"'"
+#	echo "$COMMAND"
+#	${COMMAND}
+#
+#	cp -pf $RESULT_LOGFILE $LOGFILE
+#	echo ${MAIL_LOG_CMD}
+#	${MAIL_LOG_CMD}
+#
+#	COMMAND="${SEND_MAIL_CMD} 'PUGIN TEST $NOW \"Summary results\"'"
+#	echo "$COMMAND"
+#	${COMMAND}
+#fi
 
 #
 #-- end of file --
